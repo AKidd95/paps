2fc62bb90af26d3a0c73fa5735fa7275baf1162b
Aiden_Jubelin <aiden.jubelin@gmail.com>
2018-03-21 15:45:43 +0100

    add admin to user breaks all previous routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable,
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
 end
